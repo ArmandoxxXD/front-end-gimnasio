1f623d4352a75e4a08ff26364757bfc57235f834
ArmandoxxXD <mamojose58@gmail.com>
2024-02-16 11:07:36 -0600

    deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  //apiRestURL:'http://localhost:8080',
   apiRestURL: 'https://api-rest-3.onrender.com',
-
-  //apiRestURLPagos:'http://localhost:3001/api'
   apiRestURLPagos: 'https://api-rest-3-pagos.onrender.com/api',
 };
